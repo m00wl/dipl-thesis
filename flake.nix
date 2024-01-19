da778b3537d36710e619cc121c74f8c4926a99ac
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2024-01-19 14:15:54 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "m00wl's diploma thesis";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          tex = pkgs.texlive.combine {
+            inherit (pkgs.texlive) scheme-small latexmk tudscr newunicodechar;
+          };
+        in { devShells.default = pkgs.mkShell { packages = [ tex ]; }; };
+    };
+
+  nixConfig.bash-prompt-suffix = "dev: ";
+}
